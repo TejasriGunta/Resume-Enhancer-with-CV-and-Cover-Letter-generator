32a1bb18ef1668c355653364f7ae1ee1fe285b09
TejasriGunta <tejasri1.g@gmail.com>
2025-02-17 01:28:57 +0530

    code

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from docx import Document
+from crewai import resume_enhancer_crew
+
+
+st.title("🚀 AI Resume Enhancer with Cover letter generator")
+st.markdown("Enhance your resume and generate personalized cover letters using AI.")
+
+uploaded_resume = st.file_uploader("📄 Upload Resume (Text or Docx)", type=["txt", "docx"])
+job_description = st.text_area("📝 Enter your Job Description")
+
+@st.cache_data
+def read_resume(file):
+    if file.name.endswith(".txt"):
+        return file.read().decode("utf-8", errors="ignore")
+    elif file.name.endswith(".docx"):
+        doc = Document(file)
+        return "\n".join([para.text for para in doc.paragraphs])
+    return None
+
+
+if st.button("🚀 Enhance Resume & Generate Cover Letter"):
+    if uploaded_resume and job_description:
+        resume_text = read_resume(uploaded_resume)
+        st.info("⏳ Processing... Please wait.")
+
+        # Kickoff the Crew
+        result = resume_enhancer_crew.kickoff(inputs={
+            "resume": resume_text,
+            "job_description": job_description
+        })
+
+        result_str = str(result)
+        
+        st.header("AI Resume Enhancement Results")
+
+        st.subheader("Analysis")
+        tasks_output=result.tasks_output
+        st.write(tasks_output[0].raw)   
+
+        st.subheader("CV")
+        st.write(tasks_output[1].raw) 
+
+        st.subheader("Cover Letter")
+        st.write(result.raw)
+        
+    else:
+        st.warning("⚠️ Please upload a resume and provide a job description.")
---
+++ crewai.py
@@ -0,0 +1,65 @@
+import os
+from dotenv import load_dotenv
+from crewai import Agent, Task, Crew, LLM
+
+load_dotenv()
+os.environ['GEMINI_API_KEY'] = os.getenv("GEMINI_API_KEY")
+
+# Set up the Language Model
+llm = LLM(
+    model=os.getenv("GEMINI_LLM_MODEL"),
+    verbose=True,
+    google_api_key=os.getenv("GEMINI_API_KEY")
+)
+
+# Define Agents
+agents = {
+    "Resume Analyzer": Agent(
+        name="Resume Analyzer",
+        role="Analyzes resumes for strengths, weaknesses, and ATS compatibility.",
+        goal="Provide feedback on content quality, grammatical error if present and keyword usage in the input text-{resume}.",
+         backstory="An AI-powered HR specialist with expertise in resume evaluation.",
+         allow_delegation=False,
+        llm=llm
+),
+    "CV generator": Agent(
+        name="CVGeneratorAgent",
+        role="Generate customized CVs based on resume text-{resume} and job description-{job_description}.",
+        goal="Provide a proffesional CV based on the resume text-{resume}",
+        backstory="An AI writing expert for drafting proffesional CV for jobs.",
+        allow_delegation=False,
+        llm=llm
+),
+    "Cover Letter Generator": Agent(
+        name="Cover Letter Generator",
+        role="Generates a personalized cover letter based on job description.",
+        goal="A cover letter is a short, professional letter that you send along with your resume when applying for a job. It's a way to introduce yourself to the employer and convince them to interview you. Create a compelling cover letter tailored to the job description and input resume text-{resume},{job_description}.",
+        backstory="An AI writing expert for compelling cover letters.",
+        allow_delegation=False,
+        llm=llm
+    )
+}
+tasks = {
+    "Resume Review": Task(
+        description="Analyze the following resume text for strengths, weaknesses, and grammar of resume text-{resume}",
+        agent=agents["Resume Analyzer"],
+        expected_output="Detailed review with actionable feedback.Steps to improve the resume text-{resume}"
+    ),
+    "CV": Task(
+        description="Create a proffessional CV based on the resume text-{resume} and job description-{job_description}.",
+        agent=agents["CV generator"],
+        expected_output="A professionally formatted CV with sections for personal info, education, experience, skills, and detailed information about the projects(project information text in paragraph for CV) based on the resume text-{resume}."
+    ),
+    "Cover Letter": Task(
+        description="Generate a personalized cover letter.",
+        agent=agents["Cover Letter Generator"],
+        expected_output="Well-crafted cover letter(with formal letter format with human text) tailored to the job description-{job_description}.include the following: Header-begin with a header that includes your contact details,then formal greeting with manager's name,Then Introduction-The opening paragraph of the cover letter should aim to introduce who you are and explain why you are interested in the role,After your introduction, focus on your qualifications and skills.Mention about the variety in the projects in one line(keep it short and precise).At the end conclusion. The full letter should be less than 450 words" 
+    )
+}
+
+# Assemble the Crew
+resume_enhancer_crew = Crew(
+    agents=list(agents.values()),
+    tasks=list(tasks.values())
+)
+
